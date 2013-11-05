5889f1a2130f193e2f8d94d9dc47bfe0e42aa6bb
dajiao <mck_dajiao@outlook.com>
2013-11-05 22:22:17 +0800

    from lemote

--- lexical_analysis.c
+++ lexical_analysis.c
@@ -13,7 +13,7 @@
 // NUM = digit digit*
 //________________________________________
 //
-// symbol	code	symbol	code	
+// symbol	typenum symbol	typenum	
 // **********************************
 // begin	1		:		17
 // if		2		:=		18
@@ -61,4 +61,101 @@ char input[255];
 // word buffer
 char token[255] = "";
 
+// pointer for input buffer 
+int p_input;
 
+// pointer for word buffer
+int p_token; 
+
+// current char
+char ch;
+
+// keyword array (can extend when needed)
+char* rwtab[] = {"begin", "if", "then", "while", "do", "end", _KEY_WORD_END};
+
+// lexical sanner function
+WORD* scaner();
+
+void main(){
+	
+	int over = 1;
+	WORD* oneword = new WORD;
+	printf("Enter Your words(end with #))");
+	// read the source code into buffer , end with # , multiline is avaliable
+	scanf("%[^#]s", input);		
+	p_input = 0;
+	printf("Your words:\n%s\n",input);
+	
+	// analysis the source code end with #
+	while(over<1000 && over!=-1){
+
+		// get a new word
+		oneword = scanner();
+		if(oneword->typenum < 1000){
+				// print the typenum and word
+				printf("(%d, %s)", oneword->typenum, oneword->word);
+		}
+
+		over = oneword->typenum;
+	}
+
+	printf("\npress # to exit:");
+	scanf("%[^#]s", input);
+}
+
+// get char from input buffer
+char m_getch(){
+	ch = input[p_input];
+	p_input = p_input + 1;
+	return (ch);
+}
+
+// split empty symbol
+void getbc(){
+	while(ch ==' ' || ch == 10){
+		ch = input[p_input];
+		p_input = p_input + 1;
+	}
+}
+
+// concat a word
+void concat(){
+	token[p_token] = ch;
+	p_token = p_token + 1;
+	token[p_token] = '\0';
+}
+
+// letter judge
+int letter(){
+	if(ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z') return 1;
+	else return 0;
+}
+
+// digit judge
+int digit(){
+	if(ch >= '0' && ch <= '9') return 1;
+	else return 0;
+};
+
+// reserve the keyword table
+int reserve(){
+	int i = 0;
+	while(strcmp(rwtab[i], _KEY_WORD_END)){
+		if(!strcmp(rwtab[i], token)){
+			return i+1;
+		}
+		i = i + 1;
+	}
+
+	return 10;
+}
+
+// rollback a bit
+void retract(){
+	p_input = p_input - 1;
+}
+
+// TODO digit 2 binary
+char* dtb(){
+	return NULL;
+}
